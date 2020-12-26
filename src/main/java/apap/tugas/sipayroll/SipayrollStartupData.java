0b6b7ade8ccb3d9819e6bfdbd3eba2792837f0ea
Nevil Philia <nevilphiliapramuda@gmail.com>
2020-12-27 04:51:18 +0700

    add dummy id

---
+++ src/main/java/apap/tugas/sipayroll/SipayrollStartupData.java
@@ -0,0 +1,29 @@
+package apap.tugas.sipayroll;
+
+import apap.tugas.sipayroll.model.UserModel;
+import apap.tugas.sipayroll.repository.RoleDb;
+import apap.tugas.sipayroll.service.RoleService;
+import apap.tugas.sipayroll.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class SipayrollStartupData implements ApplicationRunner {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoleDb roleDb;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if(userService.findUserByUserName("admin")==null){
+            UserModel user = new UserModel();
+            user.setUsername("admin");
+            user.setPassword("admin1234");
+            user.setRole(roleDb.findById(1));
+
+            userService.addUser(user);
+        }
+    }
+}
